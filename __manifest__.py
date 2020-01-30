35f637cd20dede65883bf13acc7c10473cf1f338
Ilaria Franchini <i.franchini@apuliasoftware.it>
2020-01-30 18:18:52 +0100

    [FIX] Correction dependes name

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,14 @@
 # Copyright 2020 Ilaria Franchini <i.franchini@apuliasoftware.it>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Fix l10n_italy_ricevute_bancarie for enterprise',
+    'name': 'Fix l10n_it_ricevute_bancarie for enterprise',
     'version': '12.0.1.0.0',
     'author': 'Apulia software',
     'category': 'Localization/Italy',
-    'summary': 'Fix l10n_italy_ricevute_bancarie for enterprise',
+    'summary': 'Fix ricevute_bancarie for enterprise',
     'website': 'http://www.apuliasoftware.it',
     'depends' : [
-                 'l10n_italy_ricevute_bancarie',
+                 'l10n_it_ricevute_bancarie',
                 ],
     'data': [
         'views/riba_view.xml'
