1e309c22d6d9b745e782fb806a55cae5b5c612ba
Ciro Urselli <c.urselli@apuliasoftware.it>
2022-08-02 11:06:29 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2020 Ilaria Franchini <i.franchini@apuliasoftware.it>
+# Copyright 2022 Apulia Software srlu <info@apuliasoftware.it>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Fix l10n_it_ricevute_bancarie for enterprise',
-    'version': '12.0.1.0.0',
+    'version': '14.0.1.0.0',
     'author': 'Apulia software',
     'category': 'Localization/Italy',
     'summary': 'Fix ricevute_bancarie for enterprise',
