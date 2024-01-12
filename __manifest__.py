a049a46a75df3e109d845688d496fd9abe7a6214
Ciro Urselli <c.urselli@apuliasoftware.it>
2024-01-12 11:38:07 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Fix l10n_it_ricevute_bancarie for enterprise',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'author': 'Apulia software',
     'category': 'Localization/Italy',
     'summary': 'Fix ricevute_bancarie for enterprise',
     'website': 'http://www.apuliasoftware.it',
     'depends' : [
-                 'l10n_it_ricevute_bancarie',
+                 'l10n_it_riba',
                 ],
     'data': [
         'views/riba_view.xml'
